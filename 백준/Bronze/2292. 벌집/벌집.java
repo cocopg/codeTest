73f4529200c77f1b160d0c88c6ec9c4a65e4219f
cocopg <124572939+cocopg@users.noreply.github.com>
2024-04-21 17:56:09 +0900

    [Bronze II] Title: 벌집, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int N =sc.nextInt();
+		
+		int roomN=1;
+		
+		int range=2;
+
+		while(range<=N) {
+			range=range+(6*roomN);
+			roomN++;
+		}
+		System.out.println(roomN);
+	}
+
+}
