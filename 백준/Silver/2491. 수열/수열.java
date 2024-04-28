ea91c4aca311a0ac65f2cf2e82baa1b334073d5e
cocopg <124572939+cocopg@users.noreply.github.com>
2024-04-28 23:35:38 +0900

    [Silver IV] Title: 수열, Time: 180 ms, Memory: 20484 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.java
+++ 백준/Silver/2491. 수열/수열.java
@@ -6,44 +6,52 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			int N = Integer.parseInt(br.readLine());
-			int[] arr = new int[N];
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int len = 1, max = 1;
-			for(int i=0; i<N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-			for(int i=0; i<N-1; i++) {
-				if(arr[i] <= arr[i+1]) {
-					len++;
-				}else {
-					if(max < len) {
-						max = len;
-					}
-					len = 1;
-				}
-			}
-			//
-			int len2 = 1, max2 = 1;
-			for(int i=0; i<N-1; i++) {
-				if(arr[i] >= arr[i+1]) {
-					len2++;
-				}else {
-					if(max2 < len2) {
-						max2 = len2;
-					}
-					len2 = 1;
-				}
-			}
-			
-			if(max2 < len2) {
-				max2 = len2;
-			}
-			if(max < len) {
-				max = len;
-			}
-			System.out.println(Math.max(max, max2));
+		
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken()); //배열에 값 넣기
 		}
+		//정배
+		int len = 1;
+		int max = 1;
+		for (int i = 0; i < N - 1; i++) {
+			if (arr[i] <= arr[i + 1]) {
+				len++;
+			} else {
+				if (max < len) {
+					max = len;
+				}
+				len = 1;
+			}
+		}
+		
+		//역배
+		int len2 = 1;
+		int max2 = 1;
+		for (int i = 0; i < N - 1; i++) {
+			if (arr[i] >= arr[i + 1]) {
+				len2++;
+			} else {
+				if (max2 < len2) {
+					max2 = len2;
+				}
+				len2 = 1;
+			}
+		}
+
+		if (max2 < len2) {
+			max2 = len2;
+		}
+		if (max < len) {
+			max = len;
+		}
+		System.out.println(Math.max(max, max2));
+	}
 }
