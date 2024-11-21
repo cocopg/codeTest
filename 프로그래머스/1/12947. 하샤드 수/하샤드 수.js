2875e9e0e518ea35438b44e744b5b543306097b4
cocopg <124572939+cocopg@users.noreply.github.com>
2024-11-22 03:21:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,10 @@
+function solution(x) {
+    var num = String(x).split("");
+    var add = 0;
+    for(let i=0;i<num.length;i++){
+        add += Number(num[i]);
+    }
+    var answer = (x%add===0?true:false);
+    
+    return answer;
+}
